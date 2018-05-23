3c39e492e5da08e58fc92523eba7aa1c16b640fa
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-23 18:23:29 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,8 @@ def reformat_languages(languages)
   languages.each do |style, info|
     info.each do |lang, data|
       new_hash[lang] = data.merge({ "style".to_sym => [style] })
+      if lang == :javascript
+        new_hash[lang][:style] << :oo
     end
   end  
 
