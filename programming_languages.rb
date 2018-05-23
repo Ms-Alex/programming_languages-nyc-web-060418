94f09bc08549fa0ec75dce801ac83b6a9a06248b
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-23 18:10:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,9 +5,9 @@ def reformat_languages(languages)
   new_hash = {}
   
   languages.each do |style, info|
-    info.merge({ :style => [style] })
-    #info.each do |lang, data|
-    new_hash = info
+    info.each do |lang, data|
+      data.merge({ :style => [style] })
+      new_hash = info
       #new_hash[info].merge({ :style => [style]})
     binding.pry
   end  
